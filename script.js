d05579c7127e06b70737b63ccdacd7c042dfff83
TejasTumsare <103879614+TejasTumsare@users.noreply.github.com>
2024-10-01 18:54:51 +0530

    Update script.js nav-list

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-
-
-
 function toggleMenu() {
     const menu = document.getElementById('nav-menu');
     const menuIcon = document.getElementById('menu-icon');
     menu.classList.toggle('show');
     menuIcon.classList.toggle('change');
 }
-
+const navLinks = document.querySelectorAll(".nav-list");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    const menu = document.getElementById("nav-menu");
+    const menuIcon = document.getElementById("menu-icon");
+    menu.classList.remove("show");
+    menuIcon.classList.remove("change");
+  });
+});
 
 function handleSubmit() {
     alert('Message Sent!');
